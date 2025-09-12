4a2939828f838b3f28b4bc0b4935a0a7179a40d8
Chet2k <chetanr@neelafilm.com>
2025-09-12 17:41:13 +0530

    Update LeafHalfAnimationScript.cs

--- Assets/Scripts/UIScripts/LeafHalfAnimationScript.cs
+++ Assets/Scripts/UIScripts/LeafHalfAnimationScript.cs
@@ -1,19 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TMKOC.SYMMETRY
 {
     public class LeafHalfAnimationScript : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        [SerializeField] private Image rightHalf;
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
